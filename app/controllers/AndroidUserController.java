a29850f008c5bc5c43d322109a5bce4c3264cd88
Andy <andypollard84@gmail.com>
2015-04-13 17:30:28 +0100

    Tidy

--- app/controllers/AndroidUserController.java
+++ app/controllers/AndroidUserController.java
@@ -1,6 +1,5 @@
 package controllers;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.List;
 
@@ -9,15 +8,11 @@ import models.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
 
 import play.libs.Json;
 import play.mvc.*;
 
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.mindrot.jbcrypt.BCrypt;
 
 
@@ -124,83 +119,4 @@ public class AndroidUserController extends Controller {
 	    	return ok(appList);
 	    }
 	}
-	
-//	public static Result getInterviews() throws JSONException, FileNotFoundException, UnsupportedEncodingException {
-//		JsonNode json = request().body().asJson();
-//		ObjectNode jsonApp = Json.newObject();
-//		String jsonString = null;
-//		
-//		if(json == null) {
-//	        return badRequest();
-//	    } else {
-//	    	ApplicantModel app = ApplicantModel.findByEmail(json.findPath("email").asText());
-//	    	if(app != null) {
-//	    		List<JobApplicationModel> jobAppList = JobApplicationModel.findAllApplicationsByUser(app);
-//	    		jsonString = "[";
-//	    		
-//	    		if(jobAppList != null) {
-//	    			for(int i=0; i< jobAppList.size(); i++) {
-//			    		jsonString += "{\"job\":{";
-//			    		jsonString += "\"app_id\":\"" + jobAppList.get(i).job_application_id + "\", ";
-//			    		jsonString += "\"job_id\":\"" + jobAppList.get(i).job.job_id + "\", ";
-//			    		jsonString += "\"job_title\":\"" + jobAppList.get(i).job.job_title + "\", ";
-//			    		jsonString += "\"job_description\":\"" + jobAppList.get(i).job.job_description + "\", ";
-//			    		jsonString += "\"job_location\":\"" + jobAppList.get(i).job.job_location + "\"";
-//			    		if(i == (jobAppList.size()-1)){
-//			    			jsonString += "}}";
-//			    		}else {
-//			    			jsonString += "}},";
-//			    		}
-//	    			}
-//	    			jsonString += "]";
-//	    		}
-//	    	}
-//	    	
-////	    	PrintWriter writer = new PrintWriter("json.txt", "UTF-8");
-////	    	writer.println(jsonString);
-////	    	writer.close();
-//	    	jsonApp = (ObjectNode) Json.parse(jsonString);
-//	    	System.out.println(jsonApp.toString());
-//	    	return ok();
-//	    }
-//	}
-	
-//	public static Result getInterviews() {
-//		JsonNode json = request().body().asJson();
-//		JSONArray questionArray = new JSONArray();
-//		JSONArray jobInterviewCollection = new JSONArray();
-//		
-//		if(json == null) {
-//	        return badRequest();
-//	    } else {
-//	    	ApplicantModel app = ApplicantModel.findByEmail(json.findPath("email").asText());
-//	    	if(app != null) {
-//	    		List<JobApplicationModel> jobAppList = JobApplicationModel.findAllApplicationsByUser(app);
-//	    		
-//	    		if(jobAppList != null) {
-//		    		for(int i=0; i< jobAppList.size(); i++) {
-//		    			JobListingModel jlm = jobAppList.get(i).job;
-//		    			ObjectNode singleJobInterview = Json.newObject();
-//		    			singleJobInterview.put("job_id", jlm.job_id);
-//		    			singleJobInterview.put("title", jlm.job_title);
-//		    			singleJobInterview.put("location", jlm.job_location);
-//		    			
-//		    			List<InterviewQuestionModel> iqList = InterviewQuestionModel.findAllQuestionsByJobListing(jlm);
-//		    			for(int j=0; j < iqList.size(); j++) {
-//		    				ObjectNode question = Json.newObject();;
-//		    				question.put("question_id", iqList.get(j).question_id);
-//		    				question.put("question", iqList.get(j).question);
-//		    				question.put("job_id", iqList.get(j).job.job_id);
-//		    				questionArray.put(question);
-//		    			}
-//		    			singleJobInterview.put("questions", questionArray.toString());
-//		    			jobInterviewCollection.put(singleJobInterview);
-//		    		}
-//	    		}
-//	    	}
-//	    	System.out.println(jobInterviewCollection);
-//	    	return ok(jobInterviewCollection.toString());
-//	    }
-//	}
-
 }
